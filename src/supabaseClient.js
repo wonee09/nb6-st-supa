f7df47e8598d69a0924acfd07245acfb80d1a802
winterview8990 <winterview8990@gmail.com>
2024-08-30 14:18:25 +0900

    d

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zymjvrcnqoymmwcujzfu.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5bWp2cmNucW95bW13Y3VqemZ1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNDk3ODEyMywiZXhwIjoyMDQwNTU0MTIzfQ.JlRjA1Yav3hGTUNV98w8yjmLIC-ZR8_J2q7eHV5oXFc";
+const supabaseUrl = "";
+const supabaseKey = "";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
